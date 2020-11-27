0187c61dc5508c8e6b050d6ada7798d429026a81
kuldeep <kuldeep.rao@ivanti.com>
2020-11-27 11:45:23 +0530

    Added latest changes

--- Console-Application-Example-2/Program.cs
+++ Console-Application-Example-2/Program.cs
@@ -6,7 +6,21 @@ namespace Console_Application_Example_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Compare Function
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            int n = string.Compare(s1, s2, true);
+            //int n = s1.CompareTo(s2);  
+            Console.Write(n);
+            Console.ReadLine();
+
+            //Contains Function
+            string s3 = Console.ReadLine();
+            string s4 = Console.ReadLine();
+            bool b;
+            b = s4.Contains(s3);
+            Console.Write(b);
+            Console.ReadLine();
         }
     }
 }
